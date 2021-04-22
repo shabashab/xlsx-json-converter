edc97832f2190c1fb7e4632c5c356508cac7831c
shabashab <shabashab@ukr.net>
2021-04-22 15:09:42 +0300

    Remove console.log from row parser

--- src/Parsers/RowParser.ts
+++ src/Parsers/RowParser.ts
@@ -19,7 +19,6 @@ export default class RowParser implements IRowParser {
       }
 
       let columnNum = parseInt(cell.col) - 1;
-      if (columnNum == 0) console.log(cell.worksheet.getColumn(cell.col));
       if (cell.isMerged && cell.master !== cell) return;
 
       let cellModel = this._cellParser.parse(cell);
